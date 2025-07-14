4f85deda0264cf07afcb1235873ef3e0b0d20951
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2025-07-15 00:08:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import studentRouter from "./routes/studentsRouter.js";
+
+const app = express()
+
+app.use(express.json())
+
+const connectionString = "mongodb+srv://admin:Ashan73646717@cluster0.szalslh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+mongoose.connect(connectionString).then(
+    ()=>{
+        console.log("Database connected")
+    }
+).catch(
+    ()=>{
+        console.log("Database connection failed")
+    }
+)
+
+
+
+app.use("/students",studentRouter)
+
+
+app.listen(6000, 
+    ()=>{
+        console.log("Server is started")
+        console.log("Thank you")
+    }
+)
