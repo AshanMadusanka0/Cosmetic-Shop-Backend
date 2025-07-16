1fc86a01b39a66923ae98e2fb2aa396c6cbfe2cd
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2025-07-16 12:39:38 +0530

    update

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { Error } from "mongoose";
 import studentRouter from "./routes/studentsRouter.js";
 import productRouter from "./routes/productsRouter.js";
 import userRouter from "./routes/userRouter.js";
+import jwt, { decode } from "jsonwebtoken"; 
 
 const app = express()
 
 app.use(express.json())
+
+//
+app.use(
+    (req,res,next)=>{
+    const token= req.header("Authorization")?.replace("Bearer","")
+    console.log(token)
+
+    if(token !=null) {
+        jwt.verify(token, "cbc-secret-key-7973",(error,
+            decoded)=>{
+             if(!error)  {
+                console.log(decoded)
+             } 
+            })
+    }
+
+        
+    }
+    
+)
+//
+
 
 const connectionString = "mongodb+srv://admin:Ashan73646717@cluster0.szalslh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
@@ -19,6 +42,9 @@ mongoose.connect(connectionString).then(
     ()=>{
         console.log("Database connection failed")
     }
+
+
+     
 )
 
 
